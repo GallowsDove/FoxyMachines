a026f1574d01329a26f45223929f6f16bc0a9f90
GallowsDove <alina.amelina@protonmail.com>
2020-12-08 14:36:45 +0100

    Fixed bug where chunk stayed loaded when destoryed by someone other than the placer, improved code.

--- src/main/java/me/gallowsdove/foxymachines/FoxyMachines.java
+++ src/main/java/me/gallowsdove/foxymachines/FoxyMachines.java
@@ -1,7 +1,6 @@
 package me.gallowsdove.foxymachines;
 
 import me.gallowsdove.foxymachines.listeners.ChunkLoaderPlaceListener;
-import me.gallowsdove.foxymachines.listeners.ChunkLoaderBreakListener;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
@@ -29,7 +28,6 @@ public class FoxyMachines extends JavaPlugin implements SlimefunAddon {
     }
 
     getServer().getPluginManager().registerEvents(new ChunkLoaderPlaceListener(), this);
-    getServer().getPluginManager().registerEvents(new ChunkLoaderBreakListener(), this);
 
     ItemSetup.INSTANCE.init();
     ResearchSetup.INSTANCE.init();
