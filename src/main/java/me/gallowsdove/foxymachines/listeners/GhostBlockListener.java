c1b69ec7305f3fedfb7ddde0522e64f30ddfeb8a
ybw0014 <i@guizhanss.com>
2022-03-15 02:06:39 -0400

    Prohibit ghost blocks from getting pulled by fishing rod (#52)
    * Prohibit ghost blocks from getting pulled by fishing rod
    
    * Apply requested changes

---
+++ src/main/java/me/gallowsdove/foxymachines/listeners/GhostBlockListener.java
@@ -0,0 +1,20 @@
+package me.gallowsdove.foxymachines.listeners;
+
+import me.gallowsdove.foxymachines.implementation.materials.GhostBlock;
+import org.bukkit.entity.FallingBlock;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerFishEvent;
+import org.bukkit.persistence.PersistentDataType;
+
+public class GhostBlockListener implements Listener {
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    private void onHitByFishingRod(PlayerFishEvent e) {
+        if (e.getCaught() instanceof FallingBlock b &&
+            b.getPersistentDataContainer().has(GhostBlock.KEY, PersistentDataType.STRING)
+        ) {
+            e.setCancelled(true);
+        }
+    }
+}
