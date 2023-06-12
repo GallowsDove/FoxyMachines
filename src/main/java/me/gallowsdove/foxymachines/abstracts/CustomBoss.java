210fda91d56f2426d0634030cf551b78b01c68a9
ybw0014 <i@guizhanss.com>
2023-06-12 12:38:13 -0400

    Fix more bossbar progress issues

--- src/main/java/me/gallowsdove/foxymachines/abstracts/CustomBoss.java
+++ src/main/java/me/gallowsdove/foxymachines/abstracts/CustomBoss.java
@@ -126,10 +126,10 @@ public abstract class CustomBoss extends CustomMob {
         BossBar bossbar = Bukkit.createBossBar(KEY, style.name, style.color, style.style, style.flags);
         bossbar.setVisible(true);
         if (entity.isInsideVehicle() && entity.getVehicle() instanceof LivingEntity vehicle) {
-            bossbar.setProgress((entity.getHealth() + vehicle.getHealth()) / (entity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() +
-                    vehicle.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue()));
+            bossbar.setProgress(Math.min((entity.getHealth() + vehicle.getHealth()) / (entity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() +
+                    vehicle.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue()), 1));
         } else {
-            bossbar.setProgress(entity.getHealth() / entity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue());
+            bossbar.setProgress(Math.min(entity.getHealth() / entity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue(), 1));
         }
         instances.put(entity, bossbar);
         return bossbar;
